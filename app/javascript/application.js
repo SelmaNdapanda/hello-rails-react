0eab51b95bc1792f4e660fa2821210d140741995
Selma <selmandapanda@yahoo.com>
2023-03-28 10:54:18 +0200

    Create the messages front-end with react-redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,48 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createAsyncThunk, createSlice, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import Greeting from './greeting';
+
+// Fetch a random message from the API endpoint
+async function getRandomMessageAPI () {
+  return fetch('http://127.0.0.1:3000/random.json')
+          .then(response => response.json())
+          .then(messageData => messageData);
+}
+
+// Create a thunk function based on the API response
+export const getRandomMessageThunk = createAsyncThunk(
+  'Messages/random',
+  getRandomMessageAPI
+);
+
+// Create the reducer/actions 
+const handleMessages = createSlice({
+  name: 'reduce',
+  initialState: {value: null},
+  extraReducers: (builder) => {
+    builder.addCase(getRandomMessageThunk.fulfilled, (state, action) => {
+      state.value = action.payload.text;
+    })
+  }
+})
+
+const store = configureStore({
+  reducer: handleMessages.reducer,
+});
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 ReactDOM.render(
