be921a852930f65c18f46893edbe0c9ef590fc45
Selma <selmandapanda@yahoo.com>
2023-03-28 10:54:46 +0200

    Update router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "root#index"
+
+  
+  get '/random', to: 'random#index'
 end
